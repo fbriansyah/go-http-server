466a2279684deca2754d1f9aad7262731756133d
Febriansyah <fbriansyah@gmail.com>
2018-08-26 16:48:40 +0700

    test request struct

--- main.go
+++ main.go
@@ -2,13 +2,25 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type anything int
 
 func (a anything) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "<h1>this is anything<h1>")
+	// fmt.Fprintln(w, "<h1>this is anything<h1>")
+	if err := r.ParseForm(); err != nil {
+		fmt.Println(err)
+	}
+
+	tpl.ExecuteTemplate(w, "index.gohtml", r.Form)
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("index.gohtml"))
 }
 
 func main() {
