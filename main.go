5255e848c728d8b05a7d2cdc861165d9c1ae5930
Febriansyah <fbriansyah@gmail.com>
2018-08-27 09:55:58 +0700

    Testing default servemux

--- main.go
+++ main.go
@@ -4,42 +4,40 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-	"net/url"
 )
-
-type hotdog int
-
-func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	err := req.ParseForm()
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	data := struct {
-		Method        string
-		URL           *url.URL
-		Submissions   map[string][]string
-		Header        http.Header
-		Host          string
-		ContentLength int64
-	}{
-		req.Method,
-		req.URL,
-		req.Form,
-		req.Header,
-		req.Host,
-		req.ContentLength,
-	}
-	tpl.ExecuteTemplate(w, "index.gohtml", data)
-}
 
 var tpl *template.Template
 
 func init() {
-	tpl = template.Must(template.ParseFiles("index.gohtml"))
+	tpl = template.Must(template.ParseGlob("templates/*"))
 }
 
 func main() {
-	var d hotdog
-	http.ListenAndServe(":8080", d)
+	http.Handle("/", http.HandlerFunc(home))
+	http.Handle("/cat/", http.HandlerFunc(cat))
+	http.Handle("/me/", http.HandlerFunc(me))
+
+	http.ListenAndServe(":8080", nil)
+}
+
+func errorHandler(w http.ResponseWriter, err error) {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		log.Println(err)
+	}
+}
+
+func home(res http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(res, "index.gohtml", nil)
+	errorHandler(res, err)
+}
+
+func cat(res http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(res, "cat.gohtml", nil)
+	errorHandler(res, err)
+}
+
+func me(res http.ResponseWriter, req *http.Request) {
+	err := tpl.ExecuteTemplate(res, "me.gohtml", "Febriansyah")
+	errorHandler(res, err)
 }
