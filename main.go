0935a4a29ae87ff3e162e8a030211290adb5211e
Febriansyah <fbriansyah@gmail.com>
2018-08-28 20:37:33 +0700

    form file upload

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
-	"log"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 var tmpl *template.Template
@@ -25,14 +29,50 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+func httpErrorHandler(w http.ResponseWriter, err error) {
+	http.Error(w, err.Error(), http.StatusInternalServerError)
+}
+
 func foo(w http.ResponseWriter, req *http.Request) {
-	f := req.FormValue("first")
-	l := req.FormValue("last")
-	s := req.FormValue("subscribe") == "on"
+	var s string
+	fmt.Println(req.Method)
+	if req.Method == http.MethodPost {
+		// open file
 
-	if err := tmpl.ExecuteTemplate(w, "index.gohtml", person{f, l, s}); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Fatalln(err)
+		f, h, err := req.FormFile("q")
+		if err != nil {
+			httpErrorHandler(w, err)
+			return
+		}
+		f.Close()
+
+		fmt.Println("\nfile:", f, "\nheader:", h, "\nerr", err)
+
+		//read
+		bs, err := ioutil.ReadAll(f)
+		if err != nil {
+			httpErrorHandler(w, err)
+			return
+		}
+		s = string(bs)
+
+		//store
+		dst, err := os.Create(filepath.Join("./storage-fs/", h.Filename))
+		if err != nil {
+			httpErrorHandler(w, err)
+			return
+		}
+
+		_, err = dst.Write(bs)
+		if err != nil {
+			httpErrorHandler(w, err)
+		}
 	}
-
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	io.WriteString(w, `
+	<form method="POST" enctype="multipart/form-data">
+	<input type="file" name="q">
+	<input type="submit">
+	</form>
+	<br>`+s)
 }
