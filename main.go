d1a8e7ccaa786b3f676b7b6ce9f0283ad3b83eb1
Febriansyah <fbriansyah@gmail.com>
2018-08-28 18:03:32 +0700

    not found handler

--- main.go
+++ main.go
@@ -1,43 +1,18 @@
 package main
 
 import (
-	"html/template"
-	"log"
+	"fmt"
 	"net/http"
 )
 
-var tpl *template.Template
-
-func init() {
-	tpl = template.Must(template.ParseGlob("templates/*"))
-}
-
 func main() {
-	http.Handle("/", http.HandlerFunc(home))
-	http.Handle("/cat/", http.HandlerFunc(cat))
-	http.Handle("/me/", http.HandlerFunc(me))
+	http.HandleFunc("/", dog)
+	http.Handle("/favicon.ico", http.NotFoundHandler())
 
 	http.ListenAndServe(":8080", nil)
 }
 
-func errorHandler(w http.ResponseWriter, err error) {
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		log.Println(err)
-	}
-}
-
-func home(res http.ResponseWriter, req *http.Request) {
-	err := tpl.ExecuteTemplate(res, "index.gohtml", nil)
-	errorHandler(res, err)
-}
-
-func cat(res http.ResponseWriter, req *http.Request) {
-	err := tpl.ExecuteTemplate(res, "cat.gohtml", nil)
-	errorHandler(res, err)
-}
-
-func me(res http.ResponseWriter, req *http.Request) {
-	err := tpl.ExecuteTemplate(res, "me.gohtml", "Febriansyah")
-	errorHandler(res, err)
+func dog(w http.ResponseWriter, req *http.Request) {
+	fmt.Println(req.URL)
+	fmt.Fprintln(w, "Lihat di terminal")
 }
